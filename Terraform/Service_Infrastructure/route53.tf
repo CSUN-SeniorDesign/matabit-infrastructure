e18def03ae91cb4bb3a4ef0469df46ac30df11b2
shahedsalehian <shahed.salehian.805@my.csun.edu>
2018-09-26 18:07:15 -0700

    added ip_headers and serverblocks; added staging to route53

--- Terraform/Service_Infrastructure/route53.tf
+++ Terraform/Service_Infrastructure/route53.tf
@@ -34,3 +34,34 @@ resource "aws_route53_record" "alb-record-apex" {
     evaluate_target_health = true
   }
 }
+resource "aws_route53_record" "alb-record-www-staging" {
+  zone_id = "${data.aws_route53_zone.selected.id}" # Replace with your zone ID
+  name    = "www.staging.matabit.org." # Replace with your name/domain/subdomain
+  type    = "A"
+
+  alias {
+    name = "${aws_lb.alb.dns_name}"
+    zone_id = "${aws_lb.alb.zone_id}"
+    evaluate_target_health = true
+  }
+}
+resource "aws_route53_record" "alb-record-blog-staging" {
+  zone_id = "${data.aws_route53_zone.selected.id}" # Replace with your zone ID
+  name    = "blog.staging.matabit.org." # Replace with your name/domain/subdomain
+  type    = "A"
+  alias {
+    name = "${aws_lb.alb.dns_name}"
+    zone_id = "${aws_lb.alb.zone_id}"
+    evaluate_target_health = true
+  }
+}
+resource "aws_route53_record" "alb-record-apex-staging" {
+  zone_id = "${data.aws_route53_zone.selected.id}" # Replace with your zone ID
+  name    = "staging.matabit.org." # Replace with your name/domain/subdomain
+  type    = "A"
+  alias {
+    name = "${aws_lb.alb.dns_name}"
+    zone_id = "${aws_lb.alb.zone_id}"
+    evaluate_target_health = true
+  }
+}
