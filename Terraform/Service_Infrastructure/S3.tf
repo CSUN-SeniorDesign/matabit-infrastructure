9599ccf1da2925688d8ea75fa9b5c4179e241404
Thomas Callier <thomas@Thomass-MacBook-Pro.local>
2018-09-28 23:30:34 -0700

    thomas week 5 blog post

---
+++ Terraform/Service_Infrastructure/S3.tf
@@ -0,0 +1,132 @@
+provider "aws" {
+  region = "us-west-2"
+}
+
+resource "aws_s3_bucket" "cci_bucket" {
+  bucket = "cci-bucket"
+  acl    = "private"
+
+  versioning {
+      enabled = true
+  }
+
+  tags {
+    Name        = "cci_bucket"
+    Environment = "Dev"
+  }
+}
+
+resource "aws_iam_role" "access" {
+    name = "cci_role_access"
+
+    assume_role_policy == <<POLICY
+{
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": "s3:ListAllMyBuckets",
+            "Resource": "arn:aws:s3:::*"
+        },
+        {
+            "Effect": "Allow",
+            "Action": "s3:*",
+            "Resource": [
+                "arn:aws:s3:::cci_bucket",
+                "arn:aws:s3:::cci_bucket/*"
+            ]
+        }
+    ]
+}
+POLICY
+}
+
+esource "aws_iam_role" "access" {
+    name = "ec2_role_access"
+
+    assume_role_policy == <<POLICY
+{
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": "s3:ListAllMyBuckets",
+            "Resource": "arn:aws:s3:::*"
+        },
+        {
+            "Effect": "Allow",
+            "Action": "s3:*",
+            "Resource": [
+                "arn:aws:s3:::cci_bucket",
+                "arn:aws:s3:::cci_bucket/*"
+            ]
+        }
+    ]
+}
+POLICY
+}
+
+
+resource "aws_iam_role" "replication" {
+  name = "tf-iam-role-replication-12345"
+
+  assume_role_policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "s3.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+POLICY
+}
+
+resource "aws_iam_policy" "replication" {
+  name = "tf-iam-role-policy-replication-12345"
+
+  policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "s3:GetReplicationConfiguration",
+        "s3:ListBucket"
+      ],
+      "Effect": "Allow",
+      "Resource": [
+        "${aws_s3_bucket.bucket.arn}"
+      ]
+    },
+    {
+      "Action": [
+        "s3:GetObjectVersion",
+        "s3:GetObjectVersionAcl"
+      ],
+      "Effect": "Allow",
+      "Resource": [
+        "${aws_s3_bucket.bucket.arn}/*"
+      ]
+    },
+    {
+      "Action": [
+        "s3:ReplicateObject",
+        "s3:ReplicateDelete"
+      ],
+      "Effect": "Allow",
+      "Resource": "${aws_s3_bucket.destination.arn}/*"
+    }
+  ]
+}
+POLICY
+}
+
+resource "aws_iam_policy_attachment" "replication" {
+  name       = "tf-iam-role-attachment-replication-12345"
+  roles      = ["${aws_iam_role.replication.name}"]
+  policy_arn = "${aws_iam_policy.replication.arn}"
+}
