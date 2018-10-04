9514cc6aca4ce908bc49f9a984c6cd5b77bc7479
digitalsoba <anthony.inlavong@gmail.com>
2018-10-03 21:19:40 -0700

    cleanup

--- Terraform/CircleCI-IAM/IAM.tf
+++ Terraform/CircleCI-IAM/IAM.tf
@@ -49,29 +49,6 @@ EOF
 }
 
 /* EC-2 GET IAM*/
-resource "aws_iam_user" "ec2-get" {
-  name = "ec2-get"
-}
-
-/* IAM Policies */
-resource "aws_iam_policy" "ec2-get" {
-  name = "ec2-get"
-
-  policy = <<EOF
-{
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Sid": "VisualEditor0",
-            "Effect": "Allow",
-            "Action": "s3:GetObject",
-            "Resource": "arn:aws:s3:::/matabit-circleci"
-        }
-    ]
-}
-EOF
-}
-
 resource "aws_iam_role" "ec2-get" {
   name = "ec2-get-role"
 
