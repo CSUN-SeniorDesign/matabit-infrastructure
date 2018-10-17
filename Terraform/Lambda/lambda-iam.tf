c5121d1046a8f5b9b68f069eb8e2eadcd9bc0059
digitalsoba <anthony.inlavong@gmail.com>
2018-10-17 01:27:53 -0700

    wow

--- Terraform/Lambda/lambda-iam.tf
+++ Terraform/Lambda/lambda-iam.tf
@@ -1,10 +1,9 @@
 resource "aws_iam_role" "lambda-ecs-role" {
   name = "lambda-ecs-role"
-
   assume_role_policy = <<EOF
-    {
-    "Version": "2012-10-17",
-    "Statement": [
+{
+  "Version": "2012-10-17",
+  "Statement": [
     {
       "Action": "sts:AssumeRole",
       "Effect": "Allow",
@@ -12,39 +11,39 @@ resource "aws_iam_role" "lambda-ecs-role" {
         "Service": "lambda.amazonaws.com"
       }
     }
-   ]
-  }
-  EOF
+  ]
+}
+EOF
 }
 
 resource "aws_iam_policy" "lambda-ecs-policy" {
   name        = "lambda-ecs-policy"
   path        = "/"
   description = "IAM policy for Lambda to update ECS and read S3"
-
   policy = <<EOF
-  {
-      "Version" : "2012-10-17"
-      "Statement": [
-          {
-              "Effect" : "Allow",
-              "Action" : [
-                  "s3:*"
-                  "cloudwatch:*",
-                  "logs:*",
-                  "lambda:*",
-                  "ecs:*",
-                  "ecr:*",
-                  "events:*"
-              ],
-              "Resouce":"*"
-          }
-      ]
-  }
-  EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": [
+        "s3:*",
+        "logs:*",
+        "cloudwatch:*",
+        "ecs:*",
+        "ecr:*",
+        "lambda:*",
+        "iam:PassRole",
+        "events:*"
+      ],
+      "Resource": "*"
+    }
+  ]
+}
+EOF
 }
 
 resource "aws_iam_role_policy_attachment" "lambda-ecs-attach" {
-  role = "${aws_iam_role.lambda-ecs-role.name}"
+  role       = "${aws_iam_role.lambda-ecs-role.name}"
   policy_arn = "${aws_iam_policy.lambda-ecs-policy.arn}"
 }
--- Terraform/Lambda/lambda.tf
+++ Terraform/Lambda/lambda.tf
@@ -11,7 +11,27 @@ provider "aws" {
   region = "${var.aws_region}"
 }
 
-resource "aws_s3_bucket_notification" "s3-bucket-notification" {
-  bucket = ""
+resource "aws_lambda_function" "update-service-lambda" {
+  filename         = "wow.zip"
+  function_name    = "update-ecs-service"
+  role             = "${aws_iam_role.lambda-ecs-role.arn}"
+  handler          = "update-ecs.main"
+  source_code_hash = "${base64sha256(file("wow.zip"))}"
+  runtime          = "python3.6"
+}
+resource "aws_lambda_permission" "allow_bucket" {
+  statement_id  = "AllowExecutionFromS3Bucket"
+  action        = "lambda:InvokeFunction"
+  function_name = "${aws_lambda_function.update-service-lambda.arn}"
+  principal     = "s3.amazonaws.com"
+  source_arn    = "${var.circle-ci-bucket-arn}"
 }
 
+resource "aws_s3_bucket_notification" "s3-bucket-notification" {
+  bucket = "${var.circle-ci-bucket-id}"
+  lambda_function{
+    lambda_function_arn = "${aws_lambda_function.update-service-lambda.arn}"
+    events              = ["s3:ObjectCreated:*"]
+    filter_suffix       = ".txt"
+  }
+}
